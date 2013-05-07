f7bad490097e9386cf4022c6b2e1c12e414b2f08
Pap Tamas <paptamas@gmail.com>
2013-05-07 18:11:28 +0300

    Implement the OAuth_Exception class.

---
+++ classes/Kohana/Oauth/Exception.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * @package		Auth Module
+ * @author      Pap Tamas
+ * @copyright   (c) 2011-2012 Pap Tamas
+ * @website		https://github.com/paptamas/kohana-auth
+ * @license		http://www.opensource.org/licenses/isc-license.txt
+ *
+ */
+
+class Kohana_OAuth_Exception extends Kohana_Exception {
+
+}
+
+// End Kohana_OAuth_Exception
---
+++ classes/Oauth/Exception.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * @package		Auth Module
+ * @author      Pap Tamas
+ * @copyright   (c) 2011-2012 Pap Tamas
+ * @website		https://github.com/paptamas/kohana-auth
+ * @license		http://www.opensource.org/licenses/isc-license.txt
+ *
+ */
+
+class OAuth_Exception extends Kohana_OAuth_Exception {
+
+}
+
+// End OAuth_Exception
