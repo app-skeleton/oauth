263d018b3e290dbbdebb3633be6af9800c8d308b
Pap Tamas <paptamas@gmail.com>
2013-05-07 18:12:03 +0300

    Add the config file for Google OAuth.

---
+++ config/oauth/google.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(	
+	'app_name'          => 'YOUR-APP-NAME', // ex. Sample app
+
+	// Go to api console (https://code.google.com/apis/console) to get these
+    'client_id'  	    => 'YOUR-CLIENT-ID',
+	'client_secret'	    => 'YOUR-CLIENT-SECRET',
+    'developer_key'	    => 'YOUR-DEVELOPER-KEY', // also known as API key
+    
+	'redirect_uri'	    => 'YOUR-REDIRECT-URI',	
+    'approval_prompt'   => 'auto' // force or auto
+);
